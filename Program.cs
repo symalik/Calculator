9c85d9238b5d9b2565ebd970de6e27b6cd3663b2
Syed <ayazsah@gmail.com>
2021-08-21 18:18:37 -0400

    Add feature to ask user to input correct integer

--- Program.cs
+++ Program.cs
@@ -16,18 +16,57 @@ namespace Calculator
             int userInput2 = 0;
             int userSelection = 0;
             int total = 0;
+            bool checkInput1 = false;
+            bool checkInput2 = false;
+            bool checkSelection = false;
 
             Console.WriteLine("This Calculator Program!");
             
-            Console.WriteLine("Please enter the first number: ");
-            userInput1 = Convert.ToInt32(Console.ReadLine());
+            //loop to check for correct input (first input)
+            while (!checkInput1)
+            {
+                Console.WriteLine("Please enter the first number: ");
+                var userInput1Check = Console.ReadLine();
 
-            Console.WriteLine("Please enter the second number: ");
-            userInput2 = Convert.ToInt32(Console.ReadLine());
+                //check input
+                checkInput1 = int.TryParse(userInput1Check, out userInput1);
 
-            Console.WriteLine("Please enter if you would like to (#):");
-            Console.WriteLine("1. Add\n2. Subtract\n3. Multiply \n4. Divide");
-            userSelection = Convert.ToInt32(Console.ReadLine());
+                if (!checkInput1)
+                {
+                    Console.WriteLine("Please enter a numerical value!");
+                }
+            }
+
+            //loop to check for correct input (second input)
+            while (!checkInput2)
+            {
+                Console.WriteLine("Please enter the second number: ");
+                var userInput2Check = Console.ReadLine();
+
+                //check input
+                checkInput2 = int.TryParse(userInput2Check, out userInput2);
+
+                if (!checkInput2)
+                {
+                    Console.WriteLine("Please enter a numerical value!");
+                }
+            }
+
+            while (!checkSelection)
+            {
+                Console.WriteLine("Please enter if you would like to (#):");
+                Console.WriteLine("1. Add\n2. Subtract\n3. Multiply \n4. Divide");
+
+                userSelection = Convert.ToInt32(Console.ReadLine());
+                
+
+                if (userSelection == 1 || userSelection == 2 || userSelection == 3 || userSelection == 4)
+                {
+                    checkSelection = true;
+                }
+
+            }
+            
 
             Console.WriteLine($"The numbers entered are {userInput1} and {userInput2} and option #{userSelection}");
 
