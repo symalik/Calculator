957c9d635edb32184e551ccdd14f3329ba39af4b
Syed <ayazsah@gmail.com>
2021-08-21 13:15:06 -0400

    Add user input functionality

--- Program.cs
+++ Program.cs
@@ -11,8 +11,19 @@ namespace Calculator
                 And thne ask the user if they want to add, subtract, multiply, or divide
                 then display the output. 
             */
+
+            int userInput1 = 0;
+            int userInput2 = 0;
+
+            Console.WriteLine("This Calculator Program!");
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the first number: ");
+            userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number: ");
+            userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The numbers entered are {userInput1} and {userInput2}");
         }
     }
 }
