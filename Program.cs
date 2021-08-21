cba30775513372bf91c1ef591eda87b6f22eb8fc
Syed <ayazsah@gmail.com>
2021-08-21 12:43:51 -0400

    Add comment to code file

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            /* 
+                Console Calculator designed to ask a user for two numbers.
+                And thne ask the user if they want to add, subtract, multiply, or divide
+                then display the output. 
+            */
+            
             Console.WriteLine("Hello World!");
         }
     }
